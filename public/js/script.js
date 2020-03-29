2ceadf0f780e74499ce87dd9b0cae85a29e5cab4
Frank Matranga <thefrankmatranga@gmail.com>
2020-03-28 22:18:46 -0400

    Landing section

--- public/js/script.js
+++ public/js/script.js
@@ -9,12 +9,4 @@ document.addEventListener('DOMContentLoaded', function () {
     //
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 
-    try {
-        let app = firebase.app();
-        let features = ['auth', 'database', 'messaging', 'storage', 'firestore', 'ete'].filter(feature => typeof app[feature] === 'function');
-        document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
-    } catch (e) {
-        console.error(e);
-        document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
-    }
 });
