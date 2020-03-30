66c76951467a05f9255241e1a299866a525dc660
Frank Matranga <thefrankmatranga@gmail.com>
2020-03-30 01:51:45 -0400

    Google auth

--- public/js/script.js
+++ public/js/script.js
@@ -8,9 +8,13 @@ document.addEventListener('DOMContentLoaded', function () {
     // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
     //
     // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+
     const app = new Vue({
         el: '#app',
         data: {
+            user: null,
             faq: [
                 {
                     question: 'What\'s a Hackathon?',
@@ -44,7 +48,16 @@ document.addEventListener('DOMContentLoaded', function () {
             events: [],
             calendar: null
         },
-        mounted () {
+        mounted() {
+            firebase.auth().onAuthStateChanged(user => {
+                this.user = user;
+                if (user) {
+                    console.log('Is logged in as ' + user.email)
+                } else {
+                    console.log('Is logged out')
+                }
+            });
+
             this.calendar = new FullCalendar.Calendar(this.$refs.calendar, {
                 plugins: ['dayGrid', 'timeGrid'],
                 defaultView: 'timeGrid',
@@ -76,8 +89,29 @@ document.addEventListener('DOMContentLoaded', function () {
                 allDaySlot: false,
                 height: '600'
             })
-    
+
             this.calendar.render()
+        },
+        methods: {
+            startGoogleAuth() {
+                firebase.auth().signInWithPopup(provider).then(function (result) {
+                    // This gives you a Google Access Token. You can use it to access the Google API.
+                    var token = result.credential.accessToken;
+                    // The signed-in user info.
+                    var user = result.user;
+                    // ...
+                    alert(user.displayName)
+                }).catch(function (error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
+                });
+            }
         }
     });
 });
