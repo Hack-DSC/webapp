a8154b2ed50ec100e14b50002176aa183d86fbc4
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-18 13:07:36 -0400

    Firestore rules and delete hacker profile

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,12 @@
-const functions = require('firebase-functions');
-const express = require('express')
-const app = express()
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+admin.initializeApp(functions.config().firebase)
 
-app.post('/sendEmail', (req,res) => {
-    res.end('Got the post')
+const db = admin.firestore()
+
+/**
+ * Delete user's hacker profile when their user account is deleted.
+ */
+exports.deleteHackerProfile = functions.auth.user().onDelete(user => {
+    return db.collection('hackers').doc(user.uid).delete()
 })
-
-exports.widgets = functions.https.onRequest(app);
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
