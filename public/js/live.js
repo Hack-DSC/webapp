5c05f804a3e094ba4a9c1a3df3b58f924e79999f
Frank Matranga <thefrankmatranga@gmail.com>
2020-04-22 14:22:23 -0400

    Live site scrollspy

--- public/js/live.js
+++ public/js/live.js
@@ -27,7 +27,15 @@ document.addEventListener('DOMContentLoaded', function () {
             
         },
         computed: {
-            
+            scheduleDayDate () {
+                return startDate.add(this.scheduleDay - 1, 'days')
+            },
+            scheduleDayDisplay () {
+                return this.scheduleDayDate.format('dddd, MMMM D')
+            },
+            sortedEvents () {
+                return this.events.sort((a, b) => a.start.toDate() - b.start.toDate())
+            }
         },
         methods: {
             async fetchData (collection) {
@@ -36,6 +44,57 @@ document.addEventListener('DOMContentLoaded', function () {
                     this[collection] = await response.json()
                 } catch (e) {
                     alert(`There was an issue getting the ${collection}.`)
+                }
+            },
+            formatTimestamp (ts) {
+                return dayjs(ts.toDate()).format('h:mm a')
+            },
+            formatTimestampDate (ts) {
+                return dayjs(ts.toDate()).format('dddd, MMMM D')
+            },
+            categoryClass (category) {
+                return {
+                    main: 'danger',
+                    workshop: 'primary',
+                    judging: 'warning',
+                    misc: 'success'
+                }[category] || 'success'
+            },
+            categoryDisplay (category) {
+                return {
+                    main: 'Main Event',
+                    workshop: 'Workshop',
+                    judging: 'Judging'
+                }[category] || 'Event'
+            },
+            debounceScheduleScroll (event) {
+                if (this.scheduleScrollTimeout) clearTimeout(this.scheduleScrollTimeout)
+                this.scheduleScrollTimeout = setTimeout(() => this.handleScheduleScroll(event), 200)
+            },
+            handleScheduleScroll (event) {
+                // Determine which day we are looking at
+                let newDay = 1
+                for (let i = 1; i <= 3; i++) {
+                    const firstDayEvent = this.sortedEvents.find(event => dayjs(event.start.toDate())
+                        .isSame(startDate
+                        .add(i - 1, 'days'), 'day'))
+                    const target = document.getElementById('event-' + firstDayEvent.id)
+                    
+                    // Are we scrolled past it??
+                    if (target.parentNode.scrollTop >= (target.offsetTop - target.parentNode.offsetTop)) {
+                        newDay = i
+                    }
+                }
+
+                this.scheduleDay = newDay
+            },
+            scrollToScheduleDay (day) {
+                const firstDayEvent = this.sortedEvents.find(event => dayjs(event.start.toDate())
+                    .isSame(startDate
+                    .add(day - 1, 'days'), 'day'))
+                if (firstDayEvent) {
+                    const target = document.getElementById('event-' + firstDayEvent.id)
+                    target.parentNode.scrollTop = target.offsetTop - target.parentNode.offsetTop
                 }
             },
         }
